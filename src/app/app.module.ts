4af79636cd079884442a15e0116bc08b223035ad
crypto netzulo <crypto.netzulo@gmail.com>
2017-12-06 19:42:24 +0100

    [qadmin]
    + added boostrap+jquery on frontend
    + TODO: page login
    + TODO: login system
    + TODO: logging system
    + TODO: make qadmin-api module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
+const routes: Routes = [
+  // {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+        enableTracing: environment.production
+      })
   ],
   providers: [],
   bootstrap: [AppComponent]
