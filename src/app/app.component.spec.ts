de540c28259281d4897316ea63a323eed3d8f733
crypto netzulo <crypto.netzulo@gmail.com>
2017-12-08 23:59:48 +0100

    [qadmin] adding loggin system for components
    + added ngx-logger
    + TODO: ngx-logger fail make fail all tests
    + fixed failed functional tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,10 +18,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('QAdmin');
   }));
-  it('should render title in a header>h1 tag', async(() => {
+  it('should render title in a header h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('header>h1').textContent).toContain('QAdmin');
+    expect(compiled.querySelector('header h1').textContent).toContain('QAdmin');
   }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 
 
 import { AppComponent } from './app.component';
@@ -16,9 +17,18 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes, {
+    RouterModule.forRoot(routes,
+      {
         enableTracing: environment.production
-      })
+      }
+    ),
+    LoggerModule.forRoot(
+      {
+        serverLoggingUrl: '/api/logs',
+        level: NgxLoggerLevel.DEBUG,
+        serverLogLevel: NgxLoggerLevel.OFF
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
